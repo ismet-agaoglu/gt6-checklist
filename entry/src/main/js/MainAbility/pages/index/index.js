4752539d5d013a29c78377b97d38d1e821e20b6c
Agayev <aga@alperagayev.com>
2026-02-15 00:31:32 +0000

    fix: use @system.fetch instead of XMLHttpRequest

--- entry/src/main/js/MainAbility/pages/index/index.js
+++ entry/src/main/js/MainAbility/pages/index/index.js
@@ -1,3 +1,5 @@
+import fetch from '@system.fetch';
+
 export default {
   data() {
     return {
@@ -5,8 +7,34 @@ export default {
       tasks: [
         { id: '1', title: 'Örnek Görev' },
         { id: '2', title: 'İkinci Görev' }
-      ]
+      ],
+      userId: 'cmlmdy7xw0000la9lcy1p0pd6',
+      apiBase: 'https://inf.alperagayev.com/api'
     };
+  },
+  onInit() {
+    this.loadTasks();
+  },
+  loadTasks() {
+    fetch.fetch({
+      url: `${this.apiBase}/tasks`,
+      method: 'GET',
+      header: { 'x-user-id': this.userId },
+      success: (res) => {
+        try {
+          let data = res.data;
+          if (typeof data === 'string') data = JSON.parse(data);
+          if (Array.isArray(data)) this.tasks = data;
+          else if (data && data.code === 200) this.tasks = data.data || [];
+          this.$forceUpdate();
+        } catch (e) {
+          console.error('parse error', e);
+        }
+      },
+      fail: (err) => {
+        console.error('fetch fail', err);
+      }
+    });
   },
   toggleAddPanel() { this.showAddPanel = !this.showAddPanel; }
 };
